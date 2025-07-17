418ac2a55b081506a82120d76f9ced8488ab6348
Daniel Appel <appel.daniel1998@gmail.com>
2025-07-17 15:47:40 +0300

    created a class to incorporate logic

---
+++ phase1/DocumentParser.py
@@ -0,0 +1,117 @@
+import json
+
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.documentintelligence import DocumentIntelligenceClient
+from azure.ai.documentintelligence.models import AnalyzeResult
+from azure.ai.documentintelligence.models import AnalyzeDocumentRequest
+from openai import AzureOpenAI
+from openai.types.chat import ChatCompletionSystemMessageParam, ChatCompletionUserMessageParam
+
+documentIntelligenceEndpoint = "https://eastus.api.cognitive.microsoft.com/"
+documentIntelligenceKey = "efbe39573b3b4e68832a5d4f6d6a391a"
+openaiEndpoint = "https://oai-lab-test-eastus-001.openai.azure.com/"
+openAiKey = "9qsKb4fIfag5vRdoqqLl1aWsH1SXJVhrtgoQdeg9XDwTXWbZMyiLJQQJ99BEACYeBjFXJ3w3AAABACOGkdkg"
+
+
+def _extractJsonContent(text):
+    """
+    Find first { and last } and extract the content between them as JSON.
+
+    :param text: String containing JSON-like content.
+    :return: Extracted JSON as dict if found, otherwise None.
+    """
+    start = text.find('{')
+    end = text.rfind('}')
+
+    if start != -1 and end != -1 and start < end:
+        return json.loads(text[start:end + 1])  # +1 to include the closing '}'
+    return None
+
+
+class DocumentParser:
+    """
+    Class to handle PDF document parsing and data extraction using Azure Document Intelligence and OpenAI.
+    """
+
+    def __init__(self):
+        self.document_intelligence_client = DocumentIntelligenceClient(
+            endpoint=documentIntelligenceEndpoint,
+            credential=AzureKeyCredential(documentIntelligenceKey)
+        )
+        self.openAiClient = AzureOpenAI(
+            azure_endpoint=openaiEndpoint,
+            api_key=openAiKey,
+            api_version="2024-12-01-preview"
+        )
+
+    def _analyzePDF(self, pdf_path):
+        """
+        Function to analyze layout and extract data from a PDF document.
+
+        Args:
+            pdf_path (str): Path to the PDF file to be analyzed.
+        """
+        try:
+            with open(pdf_path, "rb") as f:
+                document_bytes = f.read()
+        except FileNotFoundError:
+            print(f"File not found: {pdf_path}")
+            return None
+
+        poller = self.document_intelligence_client.begin_analyze_document(
+            "prebuilt-layout",
+            AnalyzeDocumentRequest(bytes_source=document_bytes),
+            features=["keyValuePairs"]
+        )
+
+        result: AnalyzeResult = poller.result()
+        return result
+
+    def _analyzeExtractedData(self, extractedData):
+        """
+        Function to analyze extracted data from a PDF document and format it into a specific JSON structure.
+        :param extractedData: The extracted data from document intelligence.
+        :return:
+        """
+
+        with open("./desiredFormatAndTranslation.txt", "r", encoding="utf-8") as f:
+            formatAndTranslation = f.read()
+        text_prompt = ("I have the following data extracted from a PDF document:\n" + str(extractedData) +
+                       "\n\nI need this data in the following json format and its translation to hebrew:\n" + formatAndTranslation +
+                       "\n\nNo need for any additional text, just return the json data (the keys in english, the values as specified in the form). " +
+                       "If the response seems inadequate or is absent, leave the values as empty strings.")
+
+        response = self.openAiClient.chat.completions.create(
+            model="gpt-4o",
+            max_tokens=4096,
+            temperature=0.3,  # Lower temperature for more deterministic output
+            top_p=0.3,  # Lower top_p for more focused output
+            messages=[
+                ChatCompletionSystemMessageParam(role="system", content="You are a helpful assistant."),
+                ChatCompletionUserMessageParam(role="user", content=text_prompt)
+            ]
+        )
+
+        firstResponse = _extractJsonContent(response.choices[0].message.content)
+
+        # First digits are not always read correctly, because there is a mark above it,
+        # but always start with 0 (all Israeli phone numbers do), so if the phone number is not empty,
+        # change the first digit to 0. If used in a different country, this logic may need to be changed.
+        if firstResponse["landlinePhone"] != "":
+            firstResponse["landlinePhone"] = '0' + firstResponse["mobilePhone"][1:]
+        if firstResponse["mobilePhone"] != "":
+            firstResponse["mobilePhone"] = '0' + firstResponse["mobilePhone"][1:]
+        return firstResponse
+
+    def runAnalysis(self, filePath: str):
+        extractedResult = self._analyzePDF(filePath)
+        if extractedResult is not None:
+            formattedData = self._analyzeExtractedData(extractedResult)
+            print(json.dumps(formattedData, indent=4, ensure_ascii=False))
+        else:
+            print("No data extracted from the PDF.")
+
+
+if __name__ == "__main__":
+    documentParser = DocumentParser()
+    documentParser.runAnalysis("../data/phase1_data/283_ex1.pdf")
